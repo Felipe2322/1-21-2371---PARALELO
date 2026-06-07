53dc5f75bdd7d0f6f4811f4d6868490573fafb9a
Felipe <felix@example.com>
2026-06-07 00:30:13 -0400

    fix: add required format to API Gateway access_log_settings

--- terraform/main.tf
+++ terraform/main.tf
@@ -203,6 +203,7 @@ resource "aws_apigatewayv2_stage" "default" {
 
   access_log_settings {
     destination_arn = aws_cloudwatch_log_group.lambda_logs.arn
+    format          = "$context.requestId $context.status $context.routeKey $context.integrationErrorMessage"
   }
 }
 
